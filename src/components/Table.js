0174aab9a5426d1140118295684a8d9143d36295
tahackr <tahacakir@hotmail.com>
2023-08-21 23:22:39 +0300

    Layout creation

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createPortal } from "react-dom";
 import {
     Table as MUITable,
@@ -12,19 +12,7 @@ import useFetch from "../hooks/useFetch";
 import Skeleton from "./Skeleton";
 
 function Table() {
-    const [isScrolled, setIsScrolled] = useState(false);
-
     const { data: coins, isFetching, error } = useFetch("/src/data/coins.json");
-
-    const handler = (e) => {
-        const { scrollLeft } = e.target;
-
-        if (scrollLeft) {
-            setIsScrolled(true);
-        } else {
-            setIsScrolled(false);
-        }
-    };
 
     if (error) {
         return createPortal(
@@ -45,47 +33,26 @@ function Table() {
     }
 
     return (
-        <MUITable
-            stickyHeader
-            onScroll={(e) => handler(e)}
-            className="!block !p-0 h-full overflow-auto"
-        >
+        <MUITable stickyHeader className="!block px-4 h-full overflow-auto">
             <TableHead>
                 <TableRow>
-                    <TableCell
-                        className={`!text-left !p-2 !font-semibold sticky left-0 top-0 !z-20 bg-white ${
-                            isScrolled && "scroll-shadow"
-                        } `}
-                    >
+                    <TableCell className="!text-left !p-2 !font-semibold sticky left-0 top-0 !z-20 bg-white">
                         Name
                     </TableCell>
                     <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
                         Price
                     </TableCell>
                     <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
-                        1h %
-                    </TableCell>
-                    <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
                         24h %
                     </TableCell>
                     <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
-                        7d %
-                    </TableCell>
-                    <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
                         Market Cap
-                    </TableCell>
-                    <TableCell className="!text-end !p-2 !font-semibold sticky top-0 z-10 bg-white">
-                        Volume 24h
                     </TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
                 {coins?.map((coin, index) => (
-                    <TableBodyRow
-                        key={`${coin.name}${index}`}
-                        coin={coin}
-                        isScrolled={isScrolled}
-                    />
+                    <TableBodyRow key={`${coin.name}${index}`} coin={coin} />
                 ))}
             </TableBody>
         </MUITable>
--- src/components/TableBodyRow.js
+++ src/components/TableBodyRow.js
@@ -3,43 +3,20 @@ import { TableRow, TableCell } from "@mui/material";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
-function TableBodyRow({ coin, isScrolled }) {
+function TableBodyRow({ coin }) {
     const {
         name,
         quote: {
-            USD: {
-                price,
-                volume_24h,
-                percent_change_1h,
-                percent_change_24h,
-                percent_change_7d,
-                market_cap,
-            },
+            USD: { price, percent_change_24h, market_cap },
         },
     } = coin;
 
     return (
         <TableRow>
-            <TableCell
-                className={`!text-left !p-2 sticky left-0 bg-white !font-semibold ${
-                    isScrolled && "scroll-shadow"
-                } `}
-            >
+            <TableCell className="!text-left !p-2 sticky left-0 bg-white !font-semibold">
                 {name}
             </TableCell>
             <TableCell className="!text-end !p-2">${price}</TableCell>
-            <TableCell
-                className={`!text-end !p-2 ${
-                    percent_change_1h > 0 ? "!text-green-600" : "!text-red-600"
-                } `}
-            >
-                {percent_change_1h < 0 ? (
-                    <ArrowDropDownIcon />
-                ) : (
-                    <ArrowDropUpIcon />
-                )}
-                {percent_change_1h.replace("-", "")}%
-            </TableCell>
             <TableCell
                 className={`!text-end !p-2 ${
                     percent_change_24h > 0 ? "!text-green-600" : "!text-red-600"
@@ -52,19 +29,6 @@ function TableBodyRow({ coin, isScrolled }) {
                 )}
                 {percent_change_24h.replace("-", "")}%
             </TableCell>
-            <TableCell
-                className={`!text-end !p-2 ${
-                    percent_change_7d > 0 ? "!text-green-600" : "!text-red-600"
-                } `}
-            >
-                {percent_change_7d < 0 ? (
-                    <ArrowDropDownIcon />
-                ) : (
-                    <ArrowDropUpIcon />
-                )}
-                {percent_change_7d.replace("-", "")}%
-            </TableCell>
-            <TableCell className="!text-end !p-2">${volume_24h}</TableCell>
             <TableCell className="!text-end !p-2">${market_cap}</TableCell>
         </TableRow>
     );
